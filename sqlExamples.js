b65df1e9796b4e375700ea565f0868598ff66895
Mykal Machon <mykalmachon@gmail.com>
2019-11-14 11:26:27 -0800

    Create Posts Table Query
    + Created a query that will create a posts table.
    + I also executed it.

--- sqlExamples.js
+++ sqlExamples.js
@@ -1,13 +1,34 @@
 const { pool } = require('./dbConfig');
 
-const createTable = () => {
-  pool.query('CREATE TABLE schools (ID SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL)', (err, res) => {
+// Create Schools Table example query
+const createSchoolsTable = () => {
+  pool.query(`CREATE TABLE schools (
+      ID SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      address VARCHAR(255) NOT NULL
+    );`, (err, res) => {
     if (err) {
       console.error(err);
     } else {
       console.log(res);
     }
   });
+}
+
+// Create Posts Table example Query
+const createPostTable = () => {
+  pool.query(`CREATE TABLE posts (
+      ID SERIAL PRIMARY KEY,
+      body VARCHAR(512) NOT NULL,
+      createDate date NOT NULL,
+      schoolId INTEGER NOT NULL REFERENCES schools(id)
+    );`, (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(res);
+    }
+  })
 }
 
 const addSchool = (name, address) => {
