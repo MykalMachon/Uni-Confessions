c8bdf6423a91e2783102cee15dac35307c5a58bb
Mykal Machon <mykalmachon@gmail.com>
2019-11-14 14:35:53 -0800

    Added New Post Functionality
    + Added new post functionality allowing users to make posts. It's not yet reflecting in the front end yet.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const schoolController = require('../controllers/schoolController');
+const postController = require('../controllers/postController');
 
 // Get the home page
 router.get('/', (req, res, next) => {
@@ -11,7 +12,10 @@ router.get('/', (req, res, next) => {
 router.get('/schools/', schoolController.getSchools);
 router.get('/schools/new', schoolController.getAddSchool);
 router.get('/schools/:id', schoolController.getSchool);
+router.get('/schools/:id/new', postController.getAddPost);
 // Post School Pages
 router.post(`/schools/new`, schoolController.addSchool);
+router.post(`/schools/:id/new`, postController.addPost);
+
 
 module.exports = router;
