dd3df429af3103fc2b5995f7005b316439cad86e
Mykal Machon <mykalmachon@gmail.com>
2019-11-19 22:23:12 -0800

    Added Device ID Cookie
    Added a year long persistent cookie, that will be used for super simple authentication

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
-// const cors = require('cors');
 const session = require('express-session');
 const cookieParser = require("cookie-parser");
 const createDOMPurify = require('dompurify');
 const { JSDOM } = require('jsdom');
-
 const indexRouter = require('./routes/index');
-
 const { Liquid } = require('liquidjs');
 const engine = new Liquid();
 const flash = require('flash');
-
+const uuidv1 = require('uuid/v1');
 const app = express();
 
 // register liquid engine
@@ -19,7 +16,10 @@ app.engine('liquid', engine.express());
 app.set('views', './views'); // specify the views directory
 app.set('view engine', 'liquid'); // set liquid to default
 
+// Show Logs when people connect
 app.use(logger('dev'));
+
+// Setup session tracking & cookies
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -38,18 +38,24 @@ app.use(
     },
   })
 );
-// app.use(cors());
+
+// Set up flashes
 app.use(flash());
 
-// clear all flashes upon re-render
+// * RUN ON ANY GET REQUEST
 app.get('/*', (req, res, next) => {
+  // clear all flashes upon re-render
   req.session.flash = [];
-
+  if (!req.cookies.deviceId) {
+    // Set device ID and set cookie age to 1 year
+    res.cookie('deviceId', uuidv1(), { maxAge: 1000 * 60 * 60 * 24 * 365 });
+  }
   next();
 });
 
-// sanitize all data sent over post requests
+// * RUN ON ANY POST REQUEST
 app.post('/*', (req, res, next) => {
+  // sanitize all data sent over post requests
   const window = (new JSDOM('')).window;
   const DOMPurify = createDOMPurify(window);
   // for each key-val in the object, loop over and sanitize values
